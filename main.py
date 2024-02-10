e84ebbfe6bb1e832af553e8a6462482194727c82
Vikng412 <kanishk.kumar412@gmail.com>
2024-02-10 08:59:07 +0530

    main.py me kuch-kuch kiya hai

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-class Person:
-    def __init__(self, x, y, z):
-        self.name = x
-        self.age = y
-        self.health = z
-    
-    def info(self):
-        print(f"{self.name} is a {self.age} years old, with {self.health} pts of health!")
-
-
-a = Person("Kanishk", "20", "100")
-a.info()
+# Measure some strings:
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(list(enumerate(words, start = 1)), len(w))
